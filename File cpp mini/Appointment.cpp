59a58442cbb1e4f26dc8aa703263818e6df26bf0
Tsaqif Jalaluddin <tsqifahmd@gmail.com>
2024-12-07 13:06:46 +0700

    Versi anyar real

---
+++ File cpp mini/Appointment.cpp
@@ -0,0 +1,22 @@
+/**
+ * Project Untitled
+ */
+
+
+#include "Appointment.h"
+
+/**
+ * Appointment implementation
+ */
+
+
+/**
+ * @param time
+ */
+void Appointment::chooseTime() {
+
+}
+
+void Appointment::confirmAvailability() {
+
+}
---
+++ File cpp mini/Bill.cpp
@@ -0,0 +1,22 @@
+/**
+ * Project Untitled
+ */
+
+
+#include "Bill.h"
+
+/**
+ * Bill implementation
+ */
+
+
+void Bill::generateBill() {
+
+}
+
+/**
+ * @param percentage
+ */
+void Bill::applyDiscount(double percentage) {
+
+}
