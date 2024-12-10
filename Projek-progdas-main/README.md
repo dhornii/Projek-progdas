8f825c5a23a7c8254db82642e38bcc78f4426f8b
Tsaqif Jalaluddin <tsqifahmd@gmail.com>
2024-12-10 22:09:49 +0700

    Delete Projek-progdas-main directory

--- Projek-progdas-main/README.md
+++
@@ -1,5 +0,0 @@
-File header projek progdas
-
-Diagram uml masih sementara, akan disesuaikan dengan kode setelah kode selesai
-
-12/4/2024
--- Projek-progdas-main/Tugas Progdas/File Header/global.h
+++
@@ -1,9 +0,0 @@
-#include "Staff.h"          //include libraries berupa Patient dan Bill
-#include "Appointment.h"    //include libraries berupa iostream dan Date.h
-#include "Bill.h"           //include libraries berupa Appointment
-#include "MedicalRecord.h"  //include libraries berupa fstream, sstream, vector, dan Prescription
-#include "Doctor.h"         //include libraries berupa Appointment, Prescription, dan MedicalRecord
-#include "Patient.h"        //include libraries berupa MedicalRecord dan payment
-#include "Payment.h"        //include libraries berupa Appointment.h
-#include "Prescription.h"   //include libraries berupa Pharmacist, list, dan string
-#include "Pharmacist.h"     //include libraries berupa string dan Prescription
